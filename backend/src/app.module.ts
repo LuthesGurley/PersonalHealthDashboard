b7f28d9eaf6aa17a2851fe5b368c4a5bde721b90
Obi, Tochukwu <66908079+LuthesGurley@users.noreply.github.com>
2025-04-16 23:37:03 +0200

    Added Entry App Module Class and main.ts . configured Mondo Db for Database Persistence

---
+++ backend/src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { HealthModule } from './health/health.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://mongo:27017/healthdb'),
+    HealthModule,
+  ],
+})
+export class AppModule {}
